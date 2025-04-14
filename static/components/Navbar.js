890ff3f4dc8096ee83dd257d08d7a6f1cf607243
hyperfi <abi00779@gmail.com>
2025-04-14 11:39:30 +0100

    more beautifications

--- static/components/Navbar.js
+++ static/components/Navbar.js
@@ -3,10 +3,10 @@ const Navbar = {
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container-fluid">
         <!-- Logo -->
-        <a class="navbar-brand d-flex align-items-center" href="#">
+        <router-link to="/" class="navbar-brand d-flex align-items-center">
           <img src="/static/images/logo.jpg" alt="Logo" width="40" height="40" class="rounded-circle me-2">
           <span class="brand-text">Event Horizon Institute</span>
-        </a>
+        </router-link>
 
         <!-- Toggler Button for Mobile -->
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
