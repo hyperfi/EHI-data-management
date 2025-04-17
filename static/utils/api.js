39a2ae60a4aaa0ea73c91483cbee09136ce1f2fa
hyperfi <abi00779@gmail.com>
2025-04-17 11:32:45 +0100

    bug fix

--- static/utils/api.js
+++ static/utils/api.js
@@ -2,21 +2,33 @@ import store from './store.js';
 
 export async function apiRequest(url, options = {}) {
   try {
-    const response = await fetch(url, options);
+      let response = await fetch(url, options);
 
-    // Check if the response status is 403
-    if (response.status === 403) {
-      // Clear session storage and dispatch logout
-      sessionStorage.removeItem('authToken');
-      sessionStorage.removeItem('isLoggedIn');
-      store.dispatch('logout');
-      console.error('Session expired. Logging out...');
-      // reload the page to redirect to login
-        window.location.reload();
-    }
-
-    return response;
-  } catch (error) {
+      console.log('Response:', response.status); // Log the response for debugging
+      // Check if the response status is 403
+      if (response.status === 403) {
+          console.log('first if')
+          window.location.reload(); // Reload the page to redirect to login
+          response = await fetch(url, options); // Retry the request
+          if (response.status === 403) {
+            console.log('second if')
+            // If still 403, clear session storage and dispatch logout
+            sessionStorage.removeItem('authToken');
+            sessionStorage.removeItem('isLoggedIn');
+            store.dispatch('logout');
+            console.error('Session expired. Logging out...');
+            window.location.reload(); // Reload the page to redirect to login
+            return; // Return to prevent further processing
+          }
+          else {
+            console.log('second else')
+            // If the response is not 403, return the response
+            return response;
+          }
+        }
+      console.log('else')
+      return response; // Return the response for further processing
+    } catch (error) {
     console.error('Error during API request:', error);
     throw error; // Re-throw the error for further handling
   }
